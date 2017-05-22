056b5f286b8280386d91be6a5f905abba56a70e1
Daniel Berrocal <jdanielb1992@gmail.com>
2017-05-22 16:48:58 -0600

    Share Data Between Components

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -1,37 +1,26 @@
 <template>
-  <h1>Hello World!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <app-user></app-user>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-}
+    import User from './components/User.vue';
+
+    export default {
+        components: {
+            appUser: User
+        }
+    }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
 </style>
