1bcc6b16e972c4409b1650ca8d7713df31d0ff53
Daniel Berrocal <jdanielb1992@gmail.com>
2017-09-10 09:14:08 -0600

    adding loading styles

--- src/components/loading/loading.vue
+++ src/components/loading/loading.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="loadingContainer">
-    <h3 class="center loadingTxt">{{message}}</h3>
+    <div class="center loadingTxt">
+      <h2 class="noMargin">{{message}}</h2>
+      <h2 class="noMargin">{{points}}</h2>
+    </div>
   </div>
 </template>
 
@@ -8,8 +11,14 @@
 export default {
   data(){
     return {
-      message: 'Loading'
+      message: 'Loading!',
+      points: ''
     }
+  },
+  mounted() {
+    setInterval(()=> {
+      this.points = this.points.includes('....') ? '.' : this.points + '.';
+    },1000);
   }
 }
 </script>
@@ -19,20 +28,28 @@ export default {
     position: fixed;
     height: 100%;
     width: 100%;
-    opacity: 0.5;
-    background-color: grey;
+    background-color: rgb(0, 0, 0);
+    /* RGBa with 0.6 opacity */
+    background-color: rgba(0, 0, 0, 0.3);
+    /* For IE 5.5 - 7*/
+    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);
+    /* For IE 8*/
+    -ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)";
     z-index: 1;
   }
 
   .center {
     position: fixed;
     top: 50%;
-    left: 45%;
   }
 
   .loadingTxt{
-    opacity: 1 !important;
-    color: #000;
+    text-align: center;
+    width: 100%;
+  }
+
+  .noMargin {
+    margin: 0;
   }
 
 </style>
